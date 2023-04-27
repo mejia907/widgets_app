4013a183a2b4ac187548d5a531f245612f0e4896
Andres Felipe Mejia <mejia9070@gmial.com>
2023-04-27 14:02:18 -0500

    configation g_router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -46,7 +47,7 @@ class _CustomListTitle extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subtitle),
       onTap: () {
-        Navigator.of(context).pushNamed(menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
