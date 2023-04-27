2102af0ec4d15fd7d65006eb08b449adf9040b72
Andres Felipe Mejia <mejia9070@gmial.com>
2023-04-27 10:44:27 -0500

    first commit

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,30 @@
+﻿import 'package:flutter/material.dart';
+
+class MenuItmes {
+  final String title;
+  final String subtitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItmes({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItmes>[
+  MenuItmes(
+    title: 'Botones',
+    subtitle: 'Varios bontones',
+    link: '/',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItmes(
+    title: 'Tarjetas',
+    subtitle: 'Un contenedor estilizado',
+    link: '/card',
+    icon: Icons.credit_card,
+  ),
+];
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,26 @@
+﻿import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.orange,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor = 0,
+  })  : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: const AppBarTheme(centerTitle: true));
+}
