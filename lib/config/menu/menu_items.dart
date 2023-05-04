99fca51dbc99be4b6415259d8d37a1a5fe3440c8
Andres Felipe Mejia <mejia9070@gmial.com>
2023-05-04 17:23:15 -0500

    animated container

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -39,4 +39,10 @@ const appMenuItems = <MenuItems>[
     link: '/snackbars',
     icon: Icons.info_outline_rounded,
   ),
+  MenuItems(
+    title: 'Animated container',
+    subtitle: 'Stateful widget animado',
+    link: '/animated',
+    icon: Icons.check_box_outline_blank_outlined,
+  ),
 ];
